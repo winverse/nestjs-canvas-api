bc470df86660c438d07c648892db5276574e003c
SangHyun Jang <public.winverse@gmail.com>
2021-11-16 18:36:34 +0900

    css fix

--- src/api/rooms/rooms.service.ts
+++ src/api/rooms/rooms.service.ts
@@ -4,25 +4,25 @@ import { Injectable } from '@nestjs/common';
 import { nanoid } from 'nanoid';
 import Utils from 'lib/utils';
 
-const names = [
-  'Eleanor Pena',
-  'Leslie Alexandar',
-  'Brooklyn Simmons',
-  'Arlene McCoy',
-  'Jerome Bell',
-  'Darlene Robertson',
-  'Kathryn Murphy',
-  'Theresa Webb',
-  'Darrerll Steward',
-  'Kylian Mbappe',
-  'David Alaba',
-  'Sergio Ramos',
-  'Thibaut Courtois',
-];
-
 @Injectable()
 export class RoomsService {
   async enter() {
+    const names = [
+      'Eleanor Pena',
+      'Leslie Alexandar',
+      'Brooklyn Simmons',
+      'Arlene McCoy',
+      'Jerome Bell',
+      'Darlene Robertson',
+      'Kathryn Murphy',
+      'Theresa Webb',
+      'Darrerll Steward',
+      'Kylian Mbappe',
+      'David Alaba',
+      'Sergio Ramos',
+      'Thibaut Courtois',
+    ];
+
     const users: UserInfo[] = (await redisGet('users')) || [];
 
     const randomNumber = Utils.randomNumber(13);
